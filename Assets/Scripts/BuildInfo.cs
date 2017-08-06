35434c360f6397bb6364e483b83a71387a37ccf7
SySTeMFa11URe <jam.morford@gmail.com>
2017-08-06 12:12:27 -0400

    Added Debug Info
    Build #0002
    +Added Build Number
    +Added XYZ Info

---
+++ Assets/Scripts/BuildInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildInfo : MonoBehaviour {
+    public Text info;
+    public GameObject player;
+    private string BuildNumber = "0002";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Backslash N for newline
+        info.text = "Icarus Technical Test \nBuild #"+BuildNumber+" \nX:"+player.transform.position.x+" Y:"+player.transform.position.y+" Z:"+player.transform.position.z;
+	}
+}
